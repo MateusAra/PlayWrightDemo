c6c0c592e56e1789dc2054f0b5c1ad5fa5c528d1
MateusAra <mateus19araujo@hotmail.com>
2023-07-16 18:07:49 -0300

    renomando namespace

--- WebTesting/Demo.cs
+++ WebTesting/Demo.cs
@@ -2,70 +2,69 @@ using System.Text.RegularExpressions;
 using Microsoft.Playwright;
 
 
-namespace Demo
-{
-    [TestFixture]
-    public class Demo
-    {
-        private IPlaywright playwright;
-        private IBrowser browser;
-        private IPage page;
+namespace PlaywrightDemo.WebTesting;
 
-        [SetUp]
-        public async Task SetUp()
+[TestFixture]
+public class Demo
+{
+    private IPlaywright playwright;
+    private IBrowser browser;
+    private IPage page;
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        playwright = await Playwright.CreateAsync();
+        browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            playwright = await Playwright.CreateAsync();
-            browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = true
-            });
-            page = await browser.NewPageAsync();
-        }
-        [Test]
-        public async Task GoToPageDocsPlaywright()
+            Headless = true
+        });
+        page = await browser.NewPageAsync();
+    }
+    [Test]
+    public async Task GoToPageDocsPlaywright()
+    {
+        //Arrange
+        await page.GotoAsync("https://playwright.dev");
+        //Action
+        var button = await page.QuerySelectorAsync("#docusaurus_skipToContent_fallback > header > div > div > a");
+        //Assert
+        if (button != null)
         {
-            //Arrange
-            await page.GotoAsync("https://playwright.dev");
-            //Action
-            var button = await page.QuerySelectorAsync("#docusaurus_skipToContent_fallback > header > div > div > a");
-            //Assert
-            if (button != null)
-            {
-                await button.ClickAsync();
-                Assert.That(await button.GetAttributeAsync("href"), Is.EqualTo("/docs/intro"));
-                Assert.That(page.Url.ToString(), Is.EqualTo("https://playwright.dev/docs/intro"));
-            }
+            await button.ClickAsync();
+            Assert.That(await button.GetAttributeAsync("href"), Is.EqualTo("/docs/intro"));
+            Assert.That(page.Url.ToString(), Is.EqualTo("https://playwright.dev/docs/intro"));
         }
-        [Test]
-        public async Task GoToLoginEaaP()
-        {
-            //Arrange
-            await page.GotoAsync("http://eaapp.somee.com");
-            await page.ClickAsync("text=Login");
-            var titleVisible = await page.Locator("h2").IsVisibleAsync();
-            //Action
-            await page.FillAsync("#UserName", "admin");
-            await page.FillAsync("#Password", "password");
-            await page.ClickAsync("text=Log in");
-            var employeeVisible = await page.Locator("text='Employee Details'").IsVisibleAsync();
-            //Assert
-            Assert.IsTrue(employeeVisible);
-            Assert.IsTrue(titleVisible);
-            Console.WriteLine($"Titulo visivel:{titleVisible}");
-            Console.WriteLine($"Employee Details visivel:{employeeVisible}");
-        }
-        [Test]
-        public async Task GoToRegisterEaaP()
-        {
-            //Arrange
-            await page.GotoAsync("http://eaapp.somee.com");
-            await page.ClickAsync("text=Register");
-            var titleVisible = await page.Locator("h2").IsVisibleAsync();
-            //Action
-           
-            //Assert
-            Assert.IsTrue(titleVisible);
-            Console.WriteLine($"Titulo visivel:{titleVisible}");
-        }
+    }
+    [Test]
+    public async Task GoToLoginEaaP()
+    {
+        //Arrange
+        await page.GotoAsync("http://eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        var titleVisible = await page.Locator("h2").IsVisibleAsync();
+        //Action
+        await page.FillAsync("#UserName", "admin");
+        await page.FillAsync("#Password", "password");
+        await page.ClickAsync("text=Log in");
+        var employeeVisible = await page.Locator("text='Employee Details'").IsVisibleAsync();
+        //Assert
+        Assert.IsTrue(employeeVisible);
+        Assert.IsTrue(titleVisible);
+        Console.WriteLine($"Titulo visivel:{titleVisible}");
+        Console.WriteLine($"Employee Details visivel:{employeeVisible}");
+    }
+    [Test]
+    public async Task GoToRegisterEaaP()
+    {
+        //Arrange
+        await page.GotoAsync("http://eaapp.somee.com");
+        await page.ClickAsync("text=Register");
+        var titleVisible = await page.Locator("h2").IsVisibleAsync();
+        //Action
+       
+        //Assert
+        Assert.IsTrue(titleVisible);
+        Console.WriteLine($"Titulo visivel:{titleVisible}");
     }
 }
