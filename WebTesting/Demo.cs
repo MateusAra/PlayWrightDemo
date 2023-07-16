3bc163c214500e3057132e328e715be1ba0938ac
MateusAra <mateus19araujo@hotmail.com>
2023-07-16 17:25:37 -0300

    CT002 - Login no Eaap

--- WebTesting/Demo.cs
+++ WebTesting/Demo.cs
@@ -23,13 +23,13 @@ namespace Demo
 
         }
         [Test]
-        public async Task HomePage()
+        public async Task GoToPageDocsPlaywright()
         {
-
+            //Arrange
             await page.GotoAsync("https://playwright.dev");
-
+            //Action
             var button = await page.QuerySelectorAsync("#docusaurus_skipToContent_fallback > header > div > div > a");
-
+            //Assert
             if (button != null)
             {
                 await button.ClickAsync();
@@ -37,6 +37,23 @@ namespace Demo
                 Assert.That(page.Url.ToString(), Is.EqualTo("https://playwright.dev/docs/intro"));
             }
         }
-
+        [Test]
+        public async Task GoToLoginEaaP()
+        {
+            //Arrange
+            await page.GotoAsync("http://eaapp.somee.com");
+            await page.ClickAsync("text=Login");
+            var titleVisible = await page.Locator("h2").IsVisibleAsync();
+            //Action
+            await page.FillAsync("#UserName", "admin");
+            await page.FillAsync("#Password", "password");
+            await page.ClickAsync("text=Log in");
+            var employeeVisible = await page.Locator("text='Employee Details'").IsVisibleAsync();
+            //Assert
+            Assert.IsTrue(employeeVisible);
+            Assert.IsTrue(titleVisible);
+            Console.WriteLine($"Titulo visivel:{titleVisible}");
+            Console.WriteLine($"Employee Details visivel:{employeeVisible}");
+        }
     }
 }
